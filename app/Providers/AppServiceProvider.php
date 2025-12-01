2cd4b93e60e6af3deba8ac3b30c91a9b251a36e2
Arturo Rivera <riveracervantesarturo@gmail.com>
2025-12-01 02:29:40 -0600

    rate-limit-apply-on-api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,10 @@ use App\Models\Sucursal;
 use App\Policies\SucursalPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,5 +37,20 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register policies
         Gate::policy(Sucursal::class, SucursalPolicy::class);
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(25)->by($request->user()?->id ?: $request->ip())
+            ->response(function (Request $request,array $headers) {
+                        // Respuesta directa y limpia para la API
+                        return response()->json(['message' => 'muchos intentos solicitados, espera para volver a intentar'],429);
+                    });
+        });
+        RateLimiter::for('api-login', function (Request $request) {
+                return Limit::perMinute(10)->by($request->input('email')?:$request->ip())
+                    ->response(function (Request $request,array $headers) {
+                        // Respuesta directa y limpia para la API
+                        return response()->json(['message' => 'muchos intentos solicitados, espera para volver a intentar'],429);
+                    });
+        });
     }
 }
